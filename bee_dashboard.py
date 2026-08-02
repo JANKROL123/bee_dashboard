6d51c38c6f9c51cb62e68cf36f872cf34441bfbf
Jan Krol <janek01krol@gmail.com>
2026-08-02 18:31:11 +0200

    Coverage displayed

--- bee_dashboard.py
+++ bee_dashboard.py
@@ -1,10 +1,60 @@
 import os
+import cv2
+import json
+import base64
+import numpy as np
+import time
 from dash import ctx, Dash, html, dcc, Input, Output, State
 from flask import send_file, request
-
+from PIL import Image
+from io import BytesIO
 ROOT = os.getcwd()
 
 app = Dash(__name__)
+
+def put_bee_frames_on_file(image_path, json_bee_data):
+
+    # Reading image and its size
+    image = cv2.imread(image_path)
+    height, width, _ = image.shape
+    frame_area = width * height
+ 
+    # Creating mask
+    mask = np.zeros((height, width), dtype=np.uint8)
+    
+    for pred in json_bee_data["predictions"]:
+        points = np.array([[p["x"], p["y"]] for p in pred["points"]], dtype=np.int32)
+        cv2.fillPoly(mask, [points], 255)
+
+    # Compute coverage
+    bee_pixels = np.count_nonzero(mask)
+    coverage = (bee_pixels / frame_area) * 100
+
+    # Visual overlay
+    overlay = image.copy()
+    overlay[mask == 255] = (0, 255, 0)
+    blended = cv2.addWeighted(image, 0.7, overlay, 0.3, 0)
+    cv2.putText(blended, f"Coverage: {coverage:.2f}%",
+        (30, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (55, 205, 55), 3)
+
+    return blended
+
+
+def numpy_to_base64(image_array):
+    if len(image_array.shape) == 3 and image_array.shape[2] == 3:
+        image_array = cv2.cvtColor(image_array, cv2.COLOR_BGR2RGB)
+    
+    # Convert to PIL Image
+    pil_image = Image.fromarray(image_array)
+    
+    # Save to BytesIO
+    buffered = BytesIO()
+    pil_image.save(buffered, format="JPEG", quality=95)
+    
+    # Encode to base64
+    img_str = base64.b64encode(buffered.getvalue()).decode()
+    
+    return f"data:image/jpeg;base64,{img_str}"
 
 
 def build_tree(path, root=ROOT):
@@ -128,8 +178,26 @@ from dash import ALL
 def serve_image():
 
     path = request.args.get("path")
+    coverage = request.args.get("coverage") == "1"
 
-    return send_file(path)
+    if not coverage:
+        return send_file(path)
+
+    json_path = os.path.splitext(path)[0] + ".json"
+
+    with open(json_path, "r") as f:
+        json_bee_data = json.load(f)
+
+    image = put_bee_frames_on_file(path, json_bee_data)
+
+
+    _, buffer = cv2.imencode(".png", image)
+
+    return send_file(
+        BytesIO(buffer.tobytes()),
+        mimetype="image/png"
+    )
+
 
 @app.callback(
     Output("selected-folder", "data"),
@@ -213,9 +281,6 @@ def show_image(images, index, coverage):
         "cursor": "pointer"
     }
 
-    if coverage:
-        style["filter"] = "brightness(0%)"
-
     return html.Div(
         [
 
@@ -226,7 +291,7 @@ def show_image(images, index, coverage):
             html.Img(
                 id="current-image",
                 n_clicks=0,
-                src=f"/image?path={image}",
+                src=f"/image?path={image}&coverage={int(coverage)}&t={time.time_ns()}",
                 style=style
             )
 
